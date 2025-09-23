f61a7521f859168cc73c0569054b0328a24d12dd
Allan Skellett <allanskellett@xmos.com>
2025-09-23 10:50:49 +0100

    VNR threshold 0.5

--- modules/lib_agc/api/agc_profiles.h
+++ modules/lib_agc/api/agc_profiles.h
@@ -49,7 +49,7 @@
     .lc_gain_double_talk = f32_to_float_s32(0), \
     .lc_gain_silence = f32_to_float_s32(0), \
     .lc_gain_min = f32_to_float_s32(0), \
-    .vnr_threshold = f32_to_float_s32(0.8),\
+    .vnr_threshold = f32_to_float_s32(0.5),\
     .vnr_low = f32_to_float_s32(0.2),\
     .vnr_low_count_limit = 17, \
     }
@@ -84,7 +84,7 @@
     .lc_gain_double_talk = f32_to_float_s32(0), \
     .lc_gain_silence = f32_to_float_s32(0), \
     .lc_gain_min = f32_to_float_s32(0), \
-    .vnr_threshold = f32_to_float_s32(0.8),\
+    .vnr_threshold = f32_to_float_s32(0.5),\
     .vnr_low = f32_to_float_s32(0.2),\
     .vnr_low_count_limit = 17, \
     }

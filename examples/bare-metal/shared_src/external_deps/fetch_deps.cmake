7ea2a3a8492eae2564a0e967f44433f115745b55
Shuchita Khare <shuchitak@xmos.com>
2026-01-16 10:54:01 +0000

    Moved file_utils to test/shared
    Move fetching xscope_fileio to file_utils/CMakeLists.txt

--- examples/bare-metal/shared_src/external_deps/fetch_deps.cmake
+++ examples/bare-metal/shared_src/external_deps/fetch_deps.cmake
@@ -9,12 +9,3 @@ FetchContent_Declare(
     SOURCE_DIR     ${CMAKE_BINARY_DIR}/fwk_voice_deps/lib_xcore_math
 )
 FetchContent_Populate(xcore_math)
-
-FetchContent_Declare(
-    xscope_fileio
-    GIT_REPOSITORY https://github.com/xmos/xscope_fileio.git
-    GIT_TAG        v1.3.1
-    GIT_SHALLOW    FALSE
-    SOURCE_DIR     ${CMAKE_BINARY_DIR}/fwk_voice_deps/xscope_fileio
-)
-FetchContent_Populate(xscope_fileio)

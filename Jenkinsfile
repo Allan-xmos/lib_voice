2227f8da1f9d15aa5bbcb2236c5cdf7c2e47ff30
uvvpavel <paveluvarov@xmos.com>
2026-03-12 13:20:58 +0000

    enabling whole non-native library build, adding and fixing test_aec_schedule

--- Jenkinsfile
+++ Jenkinsfile
@@ -167,6 +167,9 @@ pipeline {
                       dir("lib_aec/aec_unit_tests") {
                         xcoreBuild(buildDir: "build_vx4b", archiveBins: false, toolsVersion: params.TOOLS_VX4_VERSION, cmakeOpts: "-DXCORE_TARGET=XK-EVK-XU416 -DTEST_SPEEDUP_FACTOR=8")
                       }
+                      dir("lib_aec/test_aec_schedule") {
+                        xcoreBuild(buildDir: "build_vx4b", archiveBins: false, toolsVersion: params.TOOLS_VX4_VERSION, cmakeOpts: "-DXCORE_TARGET=XK-EVK-XU416")
+                      }
                       dir("lib_ns/ns_unit_tests") {
                         xcoreBuild(buildDir: "build_vx4b", archiveBins: false, toolsVersion: params.TOOLS_VX4_VERSION, cmakeOpts: "-DXCORE_TARGET=XK-EVK-XU416")
                       }
@@ -371,6 +374,12 @@ pipeline {
                         dir("lib_aec/aec_unit_tests") {
                           sh "pytest --arch vx4b --junitxml=pytest_result.xml"
                           junit "pytest_result.xml"
+                        }
+                        withEnv(["hydra_audio_PATH=/projects/hydra_audio"]) {
+                          dir("lib_aec/test_aec_schedule") {
+                            sh "pytest --arch vx4b --junitxml=pytest_result.xml"
+                            junit "pytest_result.xml"
+                          }
                         }
                         dir("lib_ns/ns_unit_tests"){
                           sh "pytest --arch vx4b --junitxml=pytest_result.xml"
--- lib_voice/lib_build_info.cmake
+++ lib_voice/lib_build_info.cmake
@@ -19,24 +19,17 @@ set(lib_ASM_SRCS "")
 include(${CMAKE_CURRENT_LIST_DIR}/vnr_model.cmake)
 file(RELATIVE_PATH MODEL_OUT_DIR_REL ${CMAKE_CURRENT_LIST_DIR} ${MODEL_OUT_DIR})
 
-file(GLOB_RECURSE LIB_C_SRCS RELATIVE ${CMAKE_CURRENT_LIST_DIR}
-                                        "${CMAKE_CURRENT_LIST_DIR}/src/aec/*.c"
-                                        "${CMAKE_CURRENT_LIST_DIR}/src/agc/*.c"
-                                        "${CMAKE_CURRENT_LIST_DIR}/src/ns/*.c"
-                                        "${CMAKE_CURRENT_LIST_DIR}/src/vnr/*.c"
-                                        )
-
 set(LIB_INCLUDES
     api
-    # api/adec
+    api/adec
     api/aec
     src/aec
     api/agc
-    # api/ic
-    # src/ic
+    api/ic
+    src/ic
     api/ns
     src/ns
-    # api/stage1
+    api/stage1
     api/vnr
     src/vnr
     ${MODEL_OUT_DIR_REL}
@@ -49,32 +42,32 @@ list(APPEND LIB_CXX_SRCS ${VNR_MODEL_SOURCES} ${VNR_CXX_SOURCES})
 
 XMOS_REGISTER_MODULE()
 
-# include(${CMAKE_CURRENT_LIST_DIR}/aec_schedule.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/aec_schedule.cmake)
 
-# # Discover schedules
-# aec_collect_schedules(single_config_sched multi_config_name multi_config_sched)
+# Discover schedules
+aec_collect_schedules(single_config_sched multi_config_name multi_config_sched)
 
-# list(LENGTH single_config_sched singleconfig_list_len)
-# list(LENGTH multi_config_sched multi_config_list_len)
+list(LENGTH single_config_sched singleconfig_list_len)
+list(LENGTH multi_config_sched multi_config_list_len)
 
 foreach(target ${APP_BUILD_TARGETS})
-#     # Apply AEC schedule if needed
-#     if(singleconfig_list_len EQUAL 1) # App only does set(AEC_SCHEDULE_CONFIG <schedule>). Attach this schedule to all targets
-#         list(GET single_config_sched 0 sched)
-#         message(VERBOSE "Target ${target} using AEC_SCHEDULE_CONFIG schedule ${sched}")
-#         generate_schedule(${target} "${sched}")
-#     elseif(multi_config_list_len GREATER 0)
-#         math(EXPR _last "${multi_config_list_len} - 1")
-#         foreach(i RANGE 0 ${_last})
-#             list(GET multi_config_name ${i} config)
-#             if(target MATCHES "${config}$") # target ends with <config>
-#                 list(GET multi_config_sched ${i} sched) # Get the corresponding schedule for this config
-#                 message(VERBOSE "aec schedule config ${config}, matches target ${target} at index ${i}. schedule = ${sched}")
-#                 generate_schedule(${target} "${sched}") # Generate schedule and add to target sources and includes
-#                 break()
-#             endif()
-#         endforeach()
-#     endif()
+    # Apply AEC schedule if needed
+    if(singleconfig_list_len EQUAL 1) # App only does set(AEC_SCHEDULE_CONFIG <schedule>). Attach this schedule to all targets
+        list(GET single_config_sched 0 sched)
+        message(VERBOSE "Target ${target} using AEC_SCHEDULE_CONFIG schedule ${sched}")
+        generate_schedule(${target} "${sched}")
+    elseif(multi_config_list_len GREATER 0)
+        math(EXPR _last "${multi_config_list_len} - 1")
+        foreach(i RANGE 0 ${_last})
+            list(GET multi_config_name ${i} config)
+            if(target MATCHES "${config}$") # target ends with <config>
+                list(GET multi_config_sched ${i} sched) # Get the corresponding schedule for this config
+                message(VERBOSE "aec schedule config ${config}, matches target ${target} at index ${i}. schedule = ${sched}")
+                generate_schedule(${target} "${sched}") # Generate schedule and add to target sources and includes
+                break()
+            endif()
+        endforeach()
+    endif()
 
     # Link aitools with the targets
     target_link_libraries(${target} PRIVATE tflite_micro)
